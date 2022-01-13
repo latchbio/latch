92a2514b6461b50c396e07e0b3a6b4f6725103b1
Kenny Workman <kennyworkman@sbcglobal.net>
2022-01-12 22:39:43 -0800

    feat: config

---
+++ latch/config.py
@@ -0,0 +1,47 @@
+"""
+config
+~~~~~~
+Repository for retrieving + updating user config values.
+"""
+
+from pathlib import Path
+
+
+class Config:
+
+    _root = None
+
+    def __init__(self):
+        self._root = Path.home().resolve().joinpath(".latch")
+
+    @property
+    def root_dir(self):
+        if self._root.exists() is False:
+            self._root.mkdir(parents=True)
+        return self._root
+
+    def token_exists(self) -> bool:
+        if self.token != "":
+            return False
+        return True
+
+    def token_valid():
+        # hit protected ping endpoint
+        ...
+
+    @property
+    def token(self) -> str:
+        """The ID token used to authorize a user in interacting with Latch.
+
+        Returns: ID token if exists else an empty string.
+        """
+        try:
+            with open(self.root_dir.joinpath("token"), "r") as f:
+                return f.read().strip()
+        except FileNotFoundError:
+            return ""
+
+    def update_token(self, token: str):
+        """Updates user config with new token regardless if one exists."""
+        with open(self.root_dir.joinpath("token"), "w") as f:
+            f.write(token)
