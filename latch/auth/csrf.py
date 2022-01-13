f07be12ca7a69b6aa7ba92d54ade83f56bd5ed55
Kenny Workman <kennyworkman@sbcglobal.net>
2022-01-12 22:41:36 -0800

    feat: csrf flow

---
+++ latch/auth/csrf.py
@@ -0,0 +1,28 @@
+"""
+csrf
+~~~~
+Context manager to hold state preventing CSRF attacks.
+"""
+
+import secrets
+
+from latch.utils import base64url_encode
+
+
+class CSRFState:
+    """A state string as a client-side protection against CSRF attacks.
+
+    Used in conjunction with PKCE verifier/challenge protection, which is a
+    server-side security mechanism.
+
+    https://spring.io/blog/2011/11/30/cross-site-request-forgery-and-oauth2
+    """
+
+    def __init__(self):
+        self.state = base64url_encode(secrets.token_bytes(32))
+
+    def __enter__(self, *args):
+        return self
+
+    def __exit__(self, *args):
+        ...
