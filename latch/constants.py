ad70edc9171a43c8d644243d4f53472850db7784
Kenny Workman <kennyworkman@sbcglobal.net>
2022-01-12 22:50:07 -0800

    feat: constants

---
+++ latch/constants.py
@@ -0,0 +1,21 @@
+"""
+constants
+~~~~~~~~~
+Library level constants for authentication.
+"""
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class OAuth2Constants:
+
+    client_id: str = "jzFBOhIbfp4EPRYZ8wmx4YyvL27LFDeB"
+    """Identifies the authentication server in 0Auth2.0 flow"""
+
+    authz_server_host: str = "https://latchai.us.auth0.com"
+    """Host of the authentication server used in 0Auth2.0 flow."""
+
+    redirect_url: str = "http://127.0.0.1:5000/callback"
+    """Redirect URL registered with authentication server."""
