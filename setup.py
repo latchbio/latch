a8bccf7f33fd36460acc72daa0c6357f5781807a
Kenny Workman <kennyworkman@sbcglobal.net>
2022-01-25 11:31:42 -0800

    feat: setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="latch",
-    version="v0.0.0",
+    version="0.0.4",
     author_email="kenny@latch.bio",
     description="latch sdk",
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "latch=latch.cli.main:main",
