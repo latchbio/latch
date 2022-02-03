928372c348c1544e69835ec9d971adcceb36f46c
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-02 23:07:01 -0800

    test: serialize + register

--- tests/test_register.py
+++ tests/test_register.py
@@ -25,7 +25,7 @@ from latch.services.register.register import (_build_image,
 
 @pytest.fixture(scope="session")
 def test_account_jwt():
-    tmp_token = ""
+    tmp_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlNMdVluMm1jTDN5UXByWjRRQ0pMdiJ9.eyJnaXZlbl9uYW1lIjoiS2VubnkiLCJmYW1pbHlfbmFtZSI6IldvcmttYW4iLCJuaWNrbmFtZSI6Imtlbm55IiwibmFtZSI6Iktlbm55IFdvcmttYW4iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2ppMVg3ZmFlaEhlemJ3MHFnQ1ZXSDUtLWpyR2tBbFpDWWxaX0sxTkRTa0hQZGFiVzhaa3UyVm9qZm1PWlNCZnB5VEQ3bngxci14cTJ1VmZhdE9Lbm05VFV3Q1FGNnU1d05kNUktMzAzZEE3aTdydUlBa3Y2dFB4VlN6YmhyTmRrYUNMZGNpN3o0aGJad0h2Nk96QVJZa1NZY2pMYkUtclFsUGpKOE9HZmpEWGgwYXl3cW1DY0RaR1NEdWw2bmdpeE5CVjd2T21ldE5VTU40bkp5NmNMSWoybF9oemVVM2k1eS15a1JUNXZlYjNrVzBQYnFMWDB1TVl2OVFhZW5YQ1dZT2MtMG9aMXMtWUxHY2UxQVREOTBTWTdUU1U1N05jZzhCVWZnczM2VzBpSm1od2FjaS03ZzlaSVVjMlFjamhxMWtBTXBNVldwUFRfSmVsejU2WHh3OF9kWFNLdGxaZ2J5eGMteENyOHBUYXJtYnlnSGFDOS1uQ2lLb1I1T2pSR2czamVTcEZxclRGV1E2NFB6NndLWkN4cTMtbjlSYnJmSU9nQjVwRjVsR3h2Q2RWNVd1MEtadm9pN1FfOFJPaUZRQ2Z6S0VMZFlJZjBCRDZNa185cEpNZ3dHNHpDcnVObmRPWUNmdDJkaWFxc3RPS2pHLXdFaEJ6TWZYZDJ5M1B1a1daSHFUd1IwQVJqN3JHNTY0dHY1cnNQeVNXMGJWZHV5OHYxQWdNR01JZ2V0UW9yeVAxNVRFSkdDM29HTm5yUHdobTJyeVhmNWNwaHdhTzFFVUMxSmhkblNmTWlGWmxhM2FRTHhDSnlHRDA0Z0JnNXlmcUJobkpLczRkQmxFX200V2JhSXF3V1lsNFZvZlNBcUFhel85T0tPRkpubnRVOFI2dnJJUGNfUFVRSFhTa3JEQWNpaFZqOF9xaTM0WVcxTTh4TmdKM2diZFZPSGlLRk1rRy1oOGo2SXVCaWNiUUFoTm1vbDR2Um1LdWRhYmFwQ3pac0ZVRWJLcEpYVUljMXFDSmc9czk2LWMiLCJsb2NhbGUiOiJlbiIsInVwZGF0ZWRfYXQiOiIyMDIyLTAyLTAxVDIzOjUwOjI2LjU0NFoiLCJlbWFpbCI6Imtlbm55QGxhdGNoLmJpbyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2xhdGNoYWkudXMuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1MDg3OTM4NjkzMDUwODkxMDI0IiwiYXVkIjoianpGQk9oSWJmcDRFUFJZWjh3bXg0WXl2TDI3TEZEZUIiLCJpYXQiOjE2NDM4Mzk0MzksImV4cCI6MTY0Mzg3NTQzOX0.YUylf3A_Ki2PP33pIZO9u0o9TtHJkU4qD1Ru-OXEIvT5lk58isKMv1R7CLyo8vCFR59nlv8e-o8xca2k31gERgjOhAu_VRmcKpX4aez3_E20jGZ3daUCU3iQOlIUckDEFxCLsd60_b2ECNXKB5Q3cHH6f3wA-EGd79UMNh4KReylauSdJnjqkLrRsA7zJRQ4D7D4DLb5d4S7DyalEJgNSIbpUw90Y5y5nqSWMj2kUPfPn0tv2mOF8IAn_5NvplmsWCapCmdPSKZvUaNCDilXEcLsDYP0psG_cjXFTZIpZShoxPsaLxDOK6cbxOmJwL2H2crk449DJ83pLBjlzpXOxQ"
     return tmp_token
 
 
@@ -37,7 +37,6 @@ def _validate_stream(stream, pkg_name, version):
 
     lines = []
     for chunk in stream:
-        print(chunk)
         lines.append(chunk)
 
     last_line = lines[-1]["stream"]
@@ -65,6 +64,7 @@ def _setup_and_build_wo_dockerfile(jwt, pkg_name, requirements=None):
         ctx = RegisterCtx(pkg_root=pkg_dir, token=jwt)
         stream = _build_image(ctx, requirements=requirements)
         _validate_stream(stream, pkg_name, _VERSION_0)
+        return ctx
 
 
 def _setup_and_build_w_dockerfile(jwt, pkg_name):
@@ -95,31 +95,57 @@ def _setup_and_build_w_dockerfile(jwt, pkg_name):
         ctx = RegisterCtx(pkg_root=pkg_dir, token=jwt)
         stream = _build_image(ctx, dockerfile=dockerfile)
         _validate_stream(stream, pkg_name, _VERSION_0)
+        return ctx
 
 
-def test_build_image_wo_dockerfile(test_account_jwt):
+# def test_build_image_wo_dockerfile(test_account_jwt):
+#
+#     _setup_and_build_wo_dockerfile(test_account_jwt, "foo")
+#     _setup_and_build_wo_dockerfile(test_account_jwt, "foo-bar")
+#
+#
+# def test_build_image_w_dockerfile(test_account_jwt):
+#
+#     _setup_and_build_w_dockerfile(test_account_jwt, "foo")
+#     _setup_and_build_w_dockerfile(test_account_jwt, "foo-bar")
+#
+#
 
-    _setup_and_build_wo_dockerfile(test_account_jwt, "foo")
-    _setup_and_build_wo_dockerfile(test_account_jwt, "foo-bar")
+# def test_serialize_pkg(test_account_jwt):
+#    def _setup_serialize(pkg):
+#        ctx = _setup_and_build_wo_dockerfile(test_account_jwt, pkg)
+#        with tempfile.TemporaryDirectory() as tmpdir:
+#            logs = _serialize_pkg(ctx, tmpdir)
+#            # Log order is shuffled
+#            assert any(["Successfully serialized" in x for x in logs[-3:]])
+#            assert pkg in logs[-3]
+#        return ctx
+#
+#    _setup_serialize("foo")
+#    _setup_serialize("foo-bar")
 
 
-def test_build_image_w_dockerfile(test_account_jwt):
+def test_image_upload(test_account_jwt):
+    def _setup_upload(pkg):
+        ctx = _setup_and_build_wo_dockerfile(test_account_jwt, pkg)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            _serialize_pkg(ctx, tmpdir)
+            logs = _upload_pkg_image(ctx)
+            for x in logs:
+                print(x, end="")
 
-    _setup_and_build_w_dockerfile(test_account_jwt, "foo")
-    _setup_and_build_w_dockerfile(test_account_jwt, "foo-bar")
-
-
-def test_serialize_pkg():
-    ...
-
-
-def test_registry_login():
-    ...
+    _setup_upload("foo")
 
 
-def test_image_upload():
-    ...
+def test_pkg_register(test_account_jwt):
+    def _setup_register(pkg):
+        ctx = _setup_and_build_wo_dockerfile(test_account_jwt, pkg)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            _serialize_pkg(ctx, tmpdir)
+            resp = _register_serialized_pkg(ctx, tmpdir)
+            stdout = resp["stdout"]
+            assert "Success" in stdout
+            assert pkg in stdout
 
-
-def test_pkg_register():
-    ...
+    _setup_register("foo")
+    _setup_register("foo-bar")
