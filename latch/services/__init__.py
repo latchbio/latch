f041bb1f2d5299759a10b84217df070c91382457
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-09 11:03:19 -0800

    doc: cp

--- latch/services/__init__.py
+++ latch/services/__init__.py
@@ -1,3 +1,10 @@
+"""Services or actions exposed through the SDK.
+
+Services take one or more domain objects and/or values and perform some
+transformation. These are the actions a user will interact with through the
+CLI, eg. to "register" a workflow given login context.
+"""
+
 from latch.services.cp import cp
 from latch.services.init import init
 from latch.services.login import login
