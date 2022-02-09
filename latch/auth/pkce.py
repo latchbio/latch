b7077c8b49b1072d9020a846d184669104e3cf92
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-09 14:40:14 -0800

    doc: pkce

--- latch/auth/pkce.py
+++ latch/auth/pkce.py
@@ -1,9 +1,3 @@
-"""
-pkce
-~~~~
-Context manager to control state for PKCE flow.
-"""
-
 import hashlib
 import secrets
 
@@ -11,7 +5,37 @@ from latch.auth.utils import base64url_encode
 
 
 class PKCE:
-    """TODO
+    """A Context manager to control state for PKCE flow.
+
+    The Proof Key for Code Exchange is outlined rigorously in `RFC7636`_.  The
+    implementation in this module was written using that document as gospel. It
+    is an extension of OAuth2.0 that adds crytographic secrets to prevent
+    request interception.
+
+    Here is a diagram to summarize the flow (capital letters correspond to
+    descriptions):
+
+    ::
+
+                                                      +-------------------+
+                                                      |   Authz Server    |
+            +--------+                                | +---------------+ |
+            |        |--(A)- Authorization Request ---->|               | |
+            |        |       + t(code_verifier), t_m  | | Authorization | |
+            |        |                                | |    Endpoint   | |
+            |        |<-(B)---- Authorization Code -----|               | |
+            |        |                                | +---------------+ |
+            | Client |                                |                   |
+            |        |                                | +---------------+ |
+            |        |--(C)-- Access Token Request ---->|               | |
+            |        |          + code_verifier       | |    Token      | |
+            |        |                                | |   Endpoint    | |
+            |        |<-(D)------ Access Token ---------|               | |
+            +--------+                                | +---------------+ |
+                                                      +-------------------+
+
+                          Figure 2: Abstract Protocol Flow
+
 
     A. The client creates and records a secret named the "code_verifier"
        and derives a transformed version "t(code_verifier)" (referred to
@@ -33,31 +57,26 @@ class PKCE:
     _redeem it for an access token, as they are not in possession of the_
     _"code_verifier" secret._
 
-                                                  +-------------------+
-                                                  |   Authz Server    |
-        +--------+                                | +---------------+ |
-        |        |--(A)- Authorization Request ---->|               | |
-        |        |       + t(code_verifier), t_m  | | Authorization | |
-        |        |                                | |    Endpoint   | |
-        |        |<-(B)---- Authorization Code -----|               | |
-        |        |                                | +---------------+ |
-        | Client |                                |                   |
-        |        |                                | +---------------+ |
-        |        |--(C)-- Access Token Request ---->|               | |
-        |        |          + code_verifier       | |    Token      | |
-        |        |                                | |   Endpoint    | |
-        |        |<-(D)------ Access Token ---------|               | |
-        +--------+                                | +---------------+ |
-                                                  +-------------------+
+    Example usage: ::
 
-                      Figure 2: Abstract Protocol Flow
+        with PKCE() as pkce:
+            oauth2_flow = OAuth2(pkce, ..)
+
+
+    .. _RFC7636:
+        https://datatracker.ietf.org/doc/html/rfc7636
     """
 
     challenge_method = "S256"
-    """If the client is capable of using "S256", it MUST use "S256", as
-       "S256" is Mandatory To Implement (MTI) on the server.
+    """The challenge method used to encode the code verifier.
 
-       RFC7636#section-4.2."""
+    ::
+        'If the client is capable of using "S256", it MUST use "S256", as
+        "S256" is Mandatory To Implement (MTI) on the server.'
+            ~ RFC7636#section-4.2.
+
+    Thus this value is hardcoded.
+    """
 
     def __init__(self):
         self.verifier, self.challenge = self.construct_challenge()
@@ -69,16 +88,18 @@ class PKCE:
         ...
 
     def construct_challenge(self) -> (str, str):
-        """A verifier + challenge to verify a client's identity in PKCE flow.
+        """Construct verifier & challenge to verify a client's identity in PKCE.
 
-        *code verifier*
-          A cryptographically random string that is used to correlate the
-          authorization request to the token request.
+        Reference  `RFC7636`_.
 
-        *code challenge*
-          A challenge derived from the code verifier that is sent in the
-          authorization request, to be verified against later.
+        Returns:
+            code verifier: A cryptographically random string that is used to
+                correlate the authorization request to the token request.
+            code challenge: A challenge derived from the code verifier that is
+            sent in the authorization request, to be verified against later.
 
+        .. _RFC7636:
+            https://datatracker.ietf.org/doc/html/rfc7636
         """
         verifier = base64url_encode(secrets.token_bytes(32))
         challenge = base64url_encode(hashlib.sha256(verifier.encode()).digest())
