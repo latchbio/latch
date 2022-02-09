2f54e4c2cf3621aa4d2bce8ef25448cc21dc4ef9
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-09 10:42:12 -0800

    docs: utils

--- latch/utils.py
+++ latch/utils.py
@@ -1,18 +1,35 @@
-"""
-utils
-~~~~~
-Hammers and nails.
-"""
+"""Utility functions for services."""
 
 import jwt
 import requests
 
 from latch.config import UserConfig
-from latch.services.login import login
+from latch.services import login
+
+
+def retrieve_or_login() -> str:
+    """Returns a valid JWT to access Latch, prompting a login flow if needed.
+
+    Returns:
+        A JWT
+    """
+
+    user_conf = UserConfig()
+    token = user_conf.token
+    if token == "" or not token_is_valid(token):
+        token = login()
+    return token
 
 
 def sub_from_jwt(token: str) -> str:
-    """Extract a user sub from a generic jwt."""
+    """Extract a user sub (UUID) from a JWT minted by auth0.
+
+    Args:
+        token: JWT
+
+    Returns:
+        The user sub contained within the JWT.
+    """
 
     payload = jwt.decode(token, options={"verify_signature": False})
     try:
@@ -25,17 +42,17 @@ def sub_from_jwt(token: str) -> str:
     return sub
 
 
-def retrieve_or_login() -> str:
-    """Returns a valid token, prompting a login flow if needed."""
-
-    user_conf = UserConfig()
-    token = user_conf.token
-    if token == "" or not token_is_valid(token):
-        token = login()
-    return token
-
-
 def token_is_valid(token: str) -> bool:
+    """Checks if passed token is authenticated with Latch.
+
+    Queries a protected endpoint within the Latch API.
+
+    Args:
+        token: JWT
+
+    Returns:
+        True if valid.
+    """
 
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(
@@ -48,6 +65,17 @@ def token_is_valid(token: str) -> bool:
 
 
 def account_id_from_token(token: str) -> str:
+    """Exchanges a valid JWT for a Latch account ID.
+
+    Latch account IDs are needed for any user-specific request, eg. register
+    workflows or copy files to Latch.
+
+    Args:
+        token: JWT
+
+    Returns:
+        A Latch account ID (UUID).
+    """
 
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.post(
