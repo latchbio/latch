738bc458d03fcb7219d196e89d50b711008aae7b
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-09 13:59:23 -0800

    docs: auth

--- latch/auth/utils.py
+++ latch/auth/utils.py
@@ -1,8 +1,4 @@
-"""
-auth.utils
-~~~~~~~~~~
-Authorization utilities.
-"""
+"""Authorization utilities."""
 
 import base64
 
@@ -10,12 +6,20 @@ import base64
 def base64url_encode(bytestring: bytes) -> str:
     """Construct a "base64url" encoding of a bytestring.
 
-    Not that this is a modified Base 64 encoding with a url + filename safe
-    alphabet. This "base64url" encoding is _not_ the same thing as a
+    Not that this is a modified Base64 encoding with a url + filename safe
+    alphabet. This "base64url" encoding is *not* the same thing as a
     "base64" encoding.
 
-    The differences are enumerated
-    [here](https://datatracker.ietf.org/doc/html/rfc4648#section-5).
+    The differences are enumerated in the `OAuth2 RFC`_.
+
+    Args:
+        bytestring: string of bytes to be encoded
+
+    Returns:
+        Encoded bytestring
+
+    .. _OAuth2 RFC:
+        https://datatracker.ietf.org/doc/html/rfc4648#section-5
     """
     # Padding must be stripped as specified in RFC.
     return base64.urlsafe_b64encode(bytestring).decode("utf-8").replace("=", "")
