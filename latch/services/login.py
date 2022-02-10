e3db00469cf228eb6bdc59c9fa8bccb62dcaa97b
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-10 10:14:14 -0800

    fix: check browser before login

--- latch/services/login.py
+++ latch/services/login.py
@@ -1,5 +1,7 @@
 """Service to login."""
 
+import webbrowser
+
 from latch.auth import PKCE, CSRFState, OAuth2
 from latch.config import UserConfig
 from latch.constants import OAuth2Constants
@@ -19,6 +21,11 @@ def login() -> str:
     .. _this RFC:
         https://datatracker.ietf.org/doc/html/rfc6749
     """
+    if _browser_available() is False:
+        raise OSError(
+            "Unable to locate a browser on this machine. Unable to facilitate"
+            " login flow"
+        )
 
     with PKCE() as pkce:
         with CSRFState() as csrf_state:
@@ -30,3 +37,21 @@ def login() -> str:
             config = UserConfig()
             config.update_token(token)
             return token
+
+
+def _browser_available() -> bool:
+    """Returns true if browser available for login flow.
+
+    Takes advantage of browser seaching logic for many platforms written
+    `here`_.
+
+    .. _here:
+        https://github.com/python/cpython/blob/main/Lib/webbrowser.py#L38
+    """
+    try:
+        browser = webbrowser.get()
+        if browser is not None:
+            return True
+    except Exception:
+        pass
+    return False
