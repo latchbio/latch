2023c62acf110e1b110c2154a323f7d89d091b96
Kenny Workman <kennyworkman@sbcglobal.net>
2022-02-12 13:10:55 -0800

    fix: enforced min python version

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+import sys
+
 from setuptools import find_packages, setup
+
+MIN_PYTHON_VERSION = (3, 7)
+CURRENT_PYTHON = sys.version_info[:2]
+
+if CURRENT_PYTHON < MIN_PYTHON_VERSION:
+    print(
+        f"Latch SDK is only supported for Python version is {MIN_PYTHON_VERSION}+. Detected you are on"
+        f" version {CURRENT_PYTHON}, installation will not proceed!"
+    )
+    sys.exit(-1)
 
 setup(
     name="latch",
@@ -6,6 +18,7 @@ setup(
     author_email="kenny@latch.bio",
     description="latch sdk",
     packages=find_packages(),
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": [
             "latch=latch.cli.main:main",
@@ -19,5 +32,10 @@ setup(
         "boto3>=1.2",
         "flaightkit==0.1.0",
         "flaightkitplugins-pod==0.0.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
