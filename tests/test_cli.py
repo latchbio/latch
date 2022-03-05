d5ad39a54cd514ebe45d4fa8f2d55489cdd1a63b
Kenny Workman <kennyworkman@sbcglobal.net>
2022-03-05 13:58:53 -0800

    fix: uncomment tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,26 +13,26 @@ def _run_and_verify(cmd: List[str], does_exist: str):
     assert does_exist in stdout
 
 
-# def test_init(test_account_jwt, project_name):
-#
-#     _cmd = ["latch", "init", project_name]
-#     _run_and_verify(_cmd, f"Created a latch workflow called {project_name}.")
-#
-#
-# def test_register(test_account_jwt, project_name):
-#
-#     _cmd = ["latch", "register", project_name]
-#     _run_and_verify(_cmd, "Successfully registered workflow.")
-#     shutil.rmtree(str(Path(project_name).resolve()))
-#
-#
-# def test_cp(test_account_jwt):
-#
-#     with open("foo.txt", "w") as f:
-#         f.write("foobar")
-#
-#     _cmd = ["latch", "cp", "foo.txt", "/foo.txt"]
-#     _run_and_verify(_cmd, "Successfully copied foo.txt to /foo.txt.")
+def test_init(test_account_jwt, project_name):
+
+    _cmd = ["latch", "init", project_name]
+    _run_and_verify(_cmd, f"Created a latch workflow called {project_name}.")
+
+
+def test_register(test_account_jwt, project_name):
+
+    _cmd = ["latch", "register", project_name]
+    _run_and_verify(_cmd, "Successfully registered workflow.")
+    shutil.rmtree(str(Path(project_name).resolve()))
+
+
+def test_cp(test_account_jwt):
+
+    with open("foo.txt", "w") as f:
+        f.write("foobar")
+
+    _cmd = ["latch", "cp", "foo.txt", "/foo.txt"]
+    _run_and_verify(_cmd, "Successfully copied foo.txt to /foo.txt.")
 
 
 def test_execute(test_account_jwt):
