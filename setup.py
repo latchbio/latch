a7fe4d1d1b3e8900f84966d479e9a025058743b7
Kenny Workman <kennyworkman@sbcglobal.net>
2022-03-08 21:35:02 -0800

    feat: v0.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if CURRENT_PYTHON < MIN_PYTHON_VERSION:
 
 setup(
     name="latch",
-    version="0.0.38",
+    version="0.1.0",
     author_email="kenny@latch.bio",
     description="latch sdk",
     packages=find_packages(),
