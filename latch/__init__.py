255431e58849bfaf384cadc176dfa4c12fb08d8c
AidanAbd <aidan@latch.bio>
2022-05-19 15:22:14 -0700

    fix: forgot import

--- latch/__init__.py
+++ latch/__init__.py
@@ -4,5 +4,5 @@ A commandline toolchain to define and register serverless workflows with the
 Latch platform.
 """
 
-from latch.resources.tasks import large_gpu_task, large_task, small_gpu_task, small_task
+from latch.resources.tasks import large_gpu_task, large_task, small_gpu_task, small_task, medium_task
 from latch.resources.workflow import workflow
