8e5a71b270ba55471bd03de84c2c630efe1b3fb0
Kenny Workman <kennyworkman@sbcglobal.net>
2022-05-26 01:28:31 -0700

    fixed tests with register

--- tests/test_register.py
+++ tests/test_register.py
@@ -17,10 +17,10 @@ from pathlib import Path
 from latch.services.init import _gen__init__
 from latch.services.register.models import RegisterCtx
 from latch.services.register.register import (
-    _build_image,
     _register_serialized_pkg,
     _serialize_pkg,
     _upload_pkg_image,
+    build_image,
 )
 
 from .fixtures import test_account_jwt
@@ -59,75 +59,15 @@ def _setup_and_build_wo_dockerfile(jwt, pkg_name, requirements=None):
             f.write(_VERSION_0)
 
         ctx = RegisterCtx(pkg_root=pkg_dir, token=jwt)
-        stream = _build_image(ctx, requirements=requirements)
+        stream = build_image(ctx, requirements=requirements)
         _validate_stream(stream, pkg_name, _VERSION_0)
         return ctx
 
 
-def _setup_and_build_w_dockerfile(jwt, pkg_name):
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-
-        pkg_dir = Path(tmpdir).joinpath(pkg_name)
-        pkg_dir.mkdir()
-        pkg_dir.joinpath("wf").mkdir()
-
-        with open(pkg_dir.joinpath("wf/__init__.py"), "w") as f:
-            f.write(_gen__init__(pkg_name))
-
-        with open(pkg_dir.joinpath("version"), "w") as f:
-            f.write(_VERSION_0)
-
-        dockerfile = Path(pkg_dir).joinpath("Dockerfile")
-        with open(dockerfile, "w") as df:
-            df.write(
-                "\n".join(
-                    [
-                        "FROM busybox",
-                        "COPY wf /src/wf",
-                        "WORKDIR /src",
-                    ]
-                )
-            )
-
-        ctx = RegisterCtx(pkg_root=pkg_dir, token=jwt)
-        stream = _build_image(ctx, dockerfile=dockerfile)
-        _validate_stream(stream, pkg_name, _VERSION_0)
-        return ctx
-
-
-def test_build_image_wo_dockerfile(test_account_jwt):
+def testbuild_image_wo_dockerfile(test_account_jwt):
 
     _setup_and_build_wo_dockerfile(test_account_jwt, "foo")
     _setup_and_build_wo_dockerfile(test_account_jwt, "foo-bar")
-
-
-def test_build_image_w_requirements(test_account_jwt):
-
-    with tempfile.NamedTemporaryFile("w") as f:
-        f.write(
-            "\n".join(
-                [
-                    "click==8.0.3",
-                    "Flask==2.0.2",
-                    "itsdangerous==2.0.1",
-                    "Jinja2==3.0.3",
-                    "MarkupSafe==2.0.1",
-                    "Werkzeug==2.0.2",
-                ]
-            )
-        )
-        f.seek(0)
-        f = Path(f.name).resolve()
-
-        _setup_and_build_wo_dockerfile(test_account_jwt, "foo", requirements=f)
-        _setup_and_build_wo_dockerfile(test_account_jwt, "foo-bar", requirements=f)
-
-
-def test_build_image_w_dockerfile(test_account_jwt):
-
-    _setup_and_build_w_dockerfile(test_account_jwt, "foo")
-    _setup_and_build_w_dockerfile(test_account_jwt, "foo-bar")
 
 
 def test_serialize_pkg(test_account_jwt):
