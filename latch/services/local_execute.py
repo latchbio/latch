c45b7ffa55d2c58783650530f188c2e625f86240
Kenny Workman <kennyworkman@sbcglobal.net>
2022-05-26 01:43:02 -0700

    handle unbuilt container

--- latch/services/local_execute.py
+++ latch/services/local_execute.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import docker
 
-from latch.services.register import RegisterCtx
+from latch.services.register import RegisterCtx, build_image
 
 
 def local_execute(pkg_root: Path):
@@ -26,26 +26,42 @@ def local_execute(pkg_root: Path):
     ctx = RegisterCtx(pkg_root)
 
     try:
-        _fetch_logs = ctx.dkr_client.get_image(ctx.full_image_tagged)
-        set(_fetch_logs)
-    except docker.errors.APIError as e:
-        raise ValueError(
-            "Unable to find an image for"
-            f"{ctx.full_image_tagged} locally. \n\nPlease register"
-            " this workflow before attempting to run locally."
-            f"\n\t $latch register {pkg_root}"
-        ) from e
+        print("\tSpinning up local container...")
+        _exec_cmd = ["python3", "/root/wf/__init__.py"]
+        container = ctx.dkr_client.create_container(
+            ctx.full_image_tagged,
+            command=_exec_cmd,
+        )
+        container_id = container.get("Id")
+        ctx.dkr_client.start(container_id)
+        logs = ctx.dkr_client.logs(container_id, stream=True)
 
-    _exec_cmd = ["python3", "/root/wf/__init__.py"]
-    container = ctx.dkr_client.create_container(
-        ctx.full_image_tagged,
-        command=_exec_cmd,
-    )
-    container_id = container.get("Id")
-    ctx.dkr_client.start(container_id)
-    logs = ctx.dkr_client.logs(container_id, stream=True)
+        print("\tStreaming stdout from registered container running locally")
+        for x in logs:
+            o = x.decode("utf-8")
+            print(f"\t\t{o}")
+    except:
+        print(
+            "\tUnable to find a local image associated with the local"
+            " workflow version."
+        )
+        _manual_build(ctx)
+        local_execute(pkg_root)
 
-    print("\tStreaming stdout from registered container running locally")
-    for x in logs:
-        o = x.decode("utf-8")
-        print(f"\t\t{o}")
+
+def _print_build_logs(build_logs, image):
+    print(f"\tBuilding Docker image for {image}")
+    for x in build_logs:
+        line = x.get("stream")
+        error = x.get("error")
+        if error is not None:
+            print(f"\t\t{x}")
+            raise OSError(f"Error when building image ~ {x}")
+        elif line is not None:
+            print(f"\t\t{line}", end="")
+
+
+def _manual_build(ctx: RegisterCtx):
+    dockerfile = ctx.pkg_root.joinpath("Dockerfile")
+    build_logs = build_image(ctx, dockerfile)
+    _print_build_logs(build_logs, ctx.full_image_tagged)
