fd08eccd345bcfeb960a6e41203a2f4ae5c2628f
Kenny Workman <kennyworkman@sbcglobal.net>
2022-05-28 21:46:48 -0700

    file_glob utility function

---
+++ latch/types/utils.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from typing import List
+from urllib.parse import urlparse
+
+from latch.types import LatchFile
+
+
+def file_glob(pattern: str, remote_path: str) -> List[LatchFile]:
+    """Constructs a list of LatchFiles from a glob pattern.
+
+    Convenient utility for passing collections of files between taks. See
+    [nextflow's channels](https://www.nextflow.io/docs/latest/channel.html) or
+    [snakemake's wildcards](https://snakemake.readthedocs.io/en/stable/snakefiles/rules.html#wildcards).
+    for similar functionality in other orchestration tools.
+
+    Args:
+        pattern: A glob pattern to match a set of files, eg. '*.py'. Will
+            resolve paths with respect to the working directory of the caller.
+        remote_path: A valid latch URL, eg. latch:///foo.txt.
+
+    Returns:
+        A list of instantiated LatchFile objects.
+
+    Intended Use: ::
+
+        @small_task
+        def task():
+
+            ...
+
+            return file_glob("*.fastq.gz", "latch:///fastqc_outputs")
+
+    """
+
+    _validate_latch_url(remote_path)
+    matched = sorted(Path(".").glob(pattern))
+    return [LatchFile(file, remote_path + file.name) for file in matched]
+
+
+class InvalidLatchURL(Exception):
+    pass
+
+
+def _validate_latch_url(url: str):
+
+    try:
+        parsed = urlparse(url)
+    except ValueError as e:
+        raise InvalidLatchURL(
+            f"{url} is not a valid url. See"
+            " https://docs.latch.bio/basics/working_with_files.html#latch-urls."
+        ) from e
+
+    if parsed.scheme != "latch":
+        raise InvalidLatchURL(
+            f"{url} is not a valid latch url - must use the 'latch' scheme. See"
+            " https://docs.latch.bio/basics/working_with_files.html#latch-urls."
+        )
+
+    if not parsed.path.startswith("/"):
+        raise InvalidLatchURL(
+            f"{url} is not a valid latch url - does not contain an absolute"
+            " path within the url. (It is common to forget the third backslash in"
+            " a the beginning of the string, eg.`latch: ///foobar.txt`, a"
+            " correctly formatted latch URL string.)"
+        )
