3caabf15197f592285a09de11a098fde928c652f
Ayush Kamat <ayush@latch.bio>
2022-05-30 10:23:42 -0700

    strip commit endpoitn

--- latch/services/register/register.py
+++ latch/services/register/register.py
@@ -5,6 +5,7 @@ import os
 import tarfile
 import tempfile
 import textwrap
+from contextlib import ExitStack
 from io import BytesIO
 from pathlib import Path
 from typing import List, Union
@@ -151,6 +152,7 @@ def register(
 
         reg_resp = _register_serialized_pkg(ctx, td_path)
         _print_reg_resp(reg_resp, ctx.image_tagged)
+        print(len(list(os.walk(td))))
 
     return RegisterOutput(
         build_logs=build_logs,
@@ -246,41 +248,17 @@ def _upload_pkg_image(ctx: RegisterCtx) -> List[str]:
 def _register_serialized_pkg(ctx: RegisterCtx, serialize_dir: Path) -> dict:
     headers = {"Authorization": f"Bearer {ctx.token}"}
 
-    serialize_files = {"version": ctx.version.encode("utf-8")}
-    for dirname, dirnames, fnames in os.walk(serialize_dir):
-        for filename in fnames + dirnames:
-            file = Path(dirname).resolve().joinpath(filename)
-            serialize_files[file.name] = open(file, "rb")
+    with ExitStack() as stack:
+        serialize_files = {"version": ctx.version.encode("utf-8")}
+        for dirname, dirnames, fnames in os.walk(serialize_dir):
+            for filename in fnames + dirnames:
+                file = Path(dirname).resolve().joinpath(filename)
+                serialize_files[file.name] = stack.enter_context(open(file, "rb"))
 
-    response = requests.post(
-        ctx.latch_register_api_url,
-        headers=headers,
-        files=serialize_files,
-    )
-
-    commit_files = {".workflow_name": ctx.pkg_root.name.encode("utf-8")}
-
-    if not (ctx.remote is None):
-        commit_files[".remote_name"] = ctx.remote.encode("utf-8")
-
-    for dirname, dirnames, fnames in os.walk(ctx.pkg_root):
-        for filename in fnames:
-            file = Path(dirname).resolve().joinpath(filename)
-            # ignore data folder
-            if ctx.pkg_root.joinpath("data") in file.parents:
-                continue
-            key = str(file.relative_to(ctx.pkg_root))
-            commit_files[key] = open(file, "rb")
-
-    commit_response = requests.post(
-        url=ctx.latch_commit_api_url,
-        headers=headers,
-        files=commit_files,
-    )
-
-    if not commit_response.json()["success"]:
-        raise ValueError(
-            "Issue committing: please make sure the specified remote exists, and that Latch can push to it."
+        response = requests.post(
+            ctx.latch_register_api_url,
+            headers=headers,
+            files=serialize_files,
         )
 
     return response.json()
