87b1614c3c424364ee05d2b313651be1b5e28678
Rohan <rohankanchana@github.com>
2022-06-22 17:53:34 -0700

    Refactor messages.py

---
+++ latch/functions/messages.py
@@ -0,0 +1,33 @@
+import os as _os
+from typing import Any, Dict
+
+import requests
+
+
+NUCLEUS_URL = _os.environ.get("LATCH_CLI_NUCLEUS_URL", "https://nucleus.latch.bio")
+ADD_MESSAGE_ENDPOINT = f"{NUCLEUS_URL}/add-task-execution-message"
+
+
+def message(typ: str, data: Dict[str, Any]) -> None:
+    try:
+        task_name = _os.environ["FLYTE_INTERNAL_TASK_NAME"]
+        task_version = _os.environ["FLYTE_INTERNAL_TASK_VERSION"]
+        execution_token = _os.environ["FLYTE_INTERNAL_EXECUTION_ID"]
+    except KeyError:
+        print(f"In local execution mode, printing below.\n[{typ}]: {data}")
+        return
+
+    response = requests.post(
+        url=ADD_MESSAGE_ENDPOINT,
+        json={
+            "execution_token": execution_token,
+            "task_name": task_name,
+            "task_version": task_version,
+            "type": typ,
+            "data": data,
+        },
+    )
+
+    if response.status_code != 200:
+        print("Error while sending message; printing message below instead...")
+        print(f"\t[{typ}]: {data}")
