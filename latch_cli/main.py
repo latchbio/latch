25ad4c8ea4f7434203812b42d94fe5eefd9d2e1f
Kenny Workman <kennyworkman@sbcglobal.net>
2022-07-16 14:49:38 -0700

    feat: flag

--- latch_cli/main.py
+++ latch_cli/main.py
@@ -28,7 +28,15 @@ def main():
     type=bool,
     help="Whether to automatically bump the version of the workflow each time register is called.",
 )
-def register(pkg_root: str, disable_auto_version: bool):
+@click.option(
+    "-r",
+    "--remote",
+    is_flag=True,
+    default=False,
+    type=bool,
+    help="Use a remote server to build workflow.",
+)
+def register(pkg_root: str, disable_auto_version: bool, remote: bool):
     """Register local workflow code to Latch.
 
     Visit docs.latch.bio to learn more.
@@ -36,7 +44,7 @@ def register(pkg_root: str, disable_auto_version: bool):
     from latch_cli.services.register import register
 
     try:
-        register(pkg_root, disable_auto_version=disable_auto_version)
+        register(pkg_root, disable_auto_version=disable_auto_version, remote=remote)
         click.secho(
             "Successfully registered workflow. View @ console.latch.bio.", fg="green"
         )
