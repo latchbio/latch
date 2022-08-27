621e8571e932d2325db72dee5d304ec39c8929ee
Ayush Kamat <ayush@latch.bio>
2022-08-27 12:59:19 -0700

    curated wf references for verified wfs
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/resources/reference_workflow.py
+++ latch/resources/reference_workflow.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from flytekit.core.launch_plan import reference_launch_plan
 
 from latch_cli.utils import current_workspace
@@ -13,3 +15,27 @@ def workflow_reference(
         name=name,
         version=version,
     )
+
+@reference_launch_plan(
+    project="1",
+    domain="development",
+    name="wf.__init__.rnaseq",
+    version="0.0.317-f53c0e",
+)
+def rnaseq(
+    samples: List[Sample],
+    alignment_quantification_tools: AlignmentTools,
+    ta_ref_genome_fork: str,
+    sa_ref_genome_fork: str,
+    output_location_fork: str,
+    run_name: str,
+    latch_genome: LatchGenome,
+    bams: List[List[LatchFile]],
+    custom_gtf: Optional[LatchFile] = None,
+    custom_ref_genome: Optional[LatchFile] = None,
+    custom_ref_trans: Optional[LatchFile] = None,
+    star_index: Optional[LatchFile] = None,
+    salmon_index: Optional[LatchFile] = None,
+    save_indices: bool = False,
+    custom_output_dir: Optional[LatchDir] = None,
+) -> List[LatchFile]:
