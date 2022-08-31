6480aefd4861de92a0a8219ff75a8be9f853f0be
Kenny Workman <kennyworkman@sbcglobal.net>
2022-08-31 16:35:16 -0700

    fix: verified imports

--- latch/verified/__init__.py
+++ latch/verified/__init__.py
@@ -1,3 +1,3 @@
-from deseq2 import deseq2_wf
-from pathway import gene_ontology_pathway_analysis
-from rnaseq import rnaseq
+from latch.verified.deseq2 import deseq2_wf
+from latch.verified.pathway import gene_ontology_pathway_analysis
+from latch.verified.rnaseq import rnaseq
