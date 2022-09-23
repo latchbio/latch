d5f8c9fc94068678c443a2aa185bcb466a82c90f
maximsmol <maximsmol@gmail.com>
2022-09-23 16:59:41 -0700

    bump version
    Signed-off-by: maximsmol <maximsmol@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if CURRENT_PYTHON < MIN_PYTHON_VERSION:
 
 setup(
     name="latch",
-    version="v2.1.2",
+    version="v2.1.3",
     author_email="kenny@latch.bio",
     description="latch sdk",
     packages=find_packages(),
