859a5aa7b775f44c4c55a637f2e9b949d9075dd8
Kenny Workman <kennyworkman@sbcglobal.net>
2022-10-19 11:02:27 -0700

    bump lytekit

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "scp>=0.14.0",
         "boto3>=1.24.22",
         "tqdm>=4.63.0",
-        "lytekit==0.12.0",
+        "lytekit==0.13.0",
         "lytekitplugins-pods==0.3.1",
         "typing-extensions==4.3.0",
         "apscheduler==3.9.1",
