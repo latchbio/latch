abeff10afba2d90d85b029e1993ec4bb757cb806
Ayush Kamat <ayush@latch.bio>
2023-01-11 11:57:34 -0800

    add small docstring
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/functions/secrets.py
+++ latch/functions/secrets.py
@@ -9,6 +9,14 @@ endpoints = config.sdk_endpoints
 
 
 def get_secret(secret_name: str):
+    """
+    A utility to allow users to reference secrets stored in their workspace on
+    Latch.
+
+    Examples:
+        >>> get_secret("test-secret")
+        "test-value-123"
+    """
     internal_execution_id = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID")
     if internal_execution_id is None:
         return _get_secret_local(secret_name)
