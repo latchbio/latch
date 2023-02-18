b6151b43dd416099a8ca37ca52d493e0c3b9c0d2
Ayush Kamat <ayush@latch.bio>
2023-02-17 16:32:15 -0800

    bump version
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if CURRENT_PYTHON < MIN_PYTHON_VERSION:
 
 setup(
     name="latch",
-    version="v2.13.0",
+    version="v2.13.1",
     author_email="kenny@latch.bio",
     description="The Latchbio SDK",
     packages=find_packages(),
