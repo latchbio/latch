41eac730ae1219440a5cf1bd9f18ef41298080d9
Ayush Kamat <ayush@latch.bio>
2023-04-18 10:57:37 -0700

    bump copyright
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information -----------------------------------------------------
 
 project = "latch"
-copyright = "2022, LatchBio"
+copyright = "2023, LatchBio"
 
 # -- General configuration ---------------------------------------------------
 
