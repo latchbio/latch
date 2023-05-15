e4780b30430bc83c785d5d9085eb27880b8c8021
Ayush Kamat <34531970+ayushkamat@users.noreply.github.com>
2023-05-15 15:57:43 -0700

    Revert "make fake import object comparable"

--- latch_cli/centromere/utils.py
+++ latch_cli/centromere/utils.py
@@ -54,28 +54,7 @@ def _import_flyte_objects(paths: List[Path], module_name: str = "wf"):
                     def __new__(*args, **kwargs):
                         return None
 
-                    def __call__(self, *args, **kwargs):
-                        return
-
-                    def __lt__(self, other):
-                        return False
-
-                    def __le__(self, other):
-                        return False
-
-                    def __eq__(self, other):
-                        return False
-
-                    def __ge__(self, other):
-                        return False
-
-                    def __gt__(self, other):
-                        return False
-
-                    def __ne__(self, other):
-                        return False
-
-                return FakeAttr()
+                return FakeAttr
 
             __all__ = []
 
