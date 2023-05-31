fd33071b1b7cd506fcac5dfb1b75fa4a152fe955
Ayush Kamat <ayush@latch.bio>
2023-05-31 12:48:37 -0700

    add deprecation warnings
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/__init__.py
+++ latch/__init__.py
@@ -4,6 +4,7 @@ A commandline toolchain to define and register serverless workflows with the
 Latch platform.
 """
 
+from latch._deprecation import _deprecated, _deprecated_import
 from latch.functions.messages import message
 from latch.functions.operators import (
     combine,
@@ -26,3 +27,36 @@ from latch.resources.tasks import (
     small_task,
 )
 from latch.resources.workflow import workflow
+
+message = _deprecated_import("message", "latch.functions.messages")(message)
+
+combine = _deprecated()(combine)
+group_tuple = _deprecated()(group_tuple)
+inner_join = _deprecated()(inner_join)
+latch_filter = _deprecated()(latch_filter)
+left_join = _deprecated()(left_join)
+outer_join = _deprecated()(outer_join)
+right_join = _deprecated()(right_join)
+
+create_conditional_section = _deprecated_import(
+    "create_conditional_section", "latch.resources.conditional"
+)(create_conditional_section)
+
+map_task = _deprecated_import("map_task", "latch.resources.map_tasks")(map_task)
+
+workflow_reference = _deprecated_import(
+    "workflow_reference", "latch.resources.reference_workflow"
+)(workflow_reference)
+
+custom_task = _deprecated_import("custom_task", "latch.resources.tasks")(custom_task)
+large_gpu_task = _deprecated_import("large_gpu_task", "latch.resources.tasks")(
+    large_gpu_task
+)
+large_task = _deprecated_import("large_task", "latch.resources.tasks")(large_task)
+medium_task = _deprecated_import("medium_task", "latch.resources.tasks")(medium_task)
+small_gpu_task = _deprecated_import("small_gpu_task", "latch.resources.tasks")(
+    small_gpu_task
+)
+small_task = _deprecated_import("small_task", "latch.resources.tasks")(small_task)
+
+workflow = _deprecated_import("workflow", "latch.resources.workflow")(workflow)
--- latch/types/__init__.py
+++ latch/types/__init__.py
@@ -1,3 +1,4 @@
+from latch._deprecation import _deprecated_import
 from latch.types.directory import LatchDir, LatchOutputDir
 from latch.types.file import LatchFile, LatchOutputFile
 from latch.types.glob import file_glob
@@ -14,3 +15,33 @@ from latch.types.metadata import (
     Spoiler,
     Text,
 )
+
+LatchDir = _deprecated_import("LatchDir", "latch.types.directory")(LatchDir)
+LatchOutputDir = _deprecated_import("LatchOutputDir", "latch.types.directory")(
+    LatchOutputDir
+)
+
+LatchFile = _deprecated_import("LatchFile", "latch.types.file")(LatchFile)
+LatchOutputFile = _deprecated_import("LatchOutputFile", "latch.types.file")(
+    LatchOutputFile
+)
+
+file_glob = _deprecated_import("file_glob", "latch.types.glob")(file_glob)
+
+Fork = _deprecated_import("Fork", "latch.types.metadata")(Fork)
+ForkBranch = _deprecated_import("ForkBranch", "latch.types.metadata")(ForkBranch)
+LatchAppearanceType = _deprecated_import("LatchAppearanceType", "latch.types.metadata")(
+    LatchAppearanceType
+)
+LatchAuthor = _deprecated_import("LatchAuthor", "latch.types.metadata")(LatchAuthor)
+LatchMetadata = _deprecated_import("LatchMetadata", "latch.types.metadata")(
+    LatchMetadata
+)
+LatchParameter = _deprecated_import("LatchParameter", "latch.types.metadata")(
+    LatchParameter
+)
+LatchRule = _deprecated_import("LatchRule", "latch.types.metadata")(LatchRule)
+Params = _deprecated_import("Params", "latch.types.metadata")(Params)
+Section = _deprecated_import("Section", "latch.types.metadata")(Section)
+Spoiler = _deprecated_import("Spoiler", "latch.types.metadata")(Spoiler)
+Text = _deprecated_import("Text", "latch.types.metadata")(Text)
