8cc6ad7555ebb42ecaa6ff4cc18a444310f01a43
Ayush Kamat <ayush@latch.bio>
2023-06-06 10:28:55 -0700

    shtuff
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- setup.py
+++ setup.py
@@ -2,31 +2,29 @@ import sys
 
 from setuptools import find_packages, setup
 
-MIN_PYTHON_VERSION = (3, 8)
-CURRENT_PYTHON = sys.version_info[:2]
+cur_ver = sys.version_info[:2]
+ver_str = ".".join(map(str, cur_ver))
 
-if CURRENT_PYTHON < MIN_PYTHON_VERSION:
-    print(
-        f"Latch SDK is only supported for Python version is {MIN_PYTHON_VERSION}+."
-        f" Detected you are on version {CURRENT_PYTHON}, installation will not proceed!"
+if cur_ver < (3, 8) or cur_ver > (3, 10):
+    raise RuntimeError(
+        f"Python {ver_str} is unsupported. Please use a Python version between 3.8 and"
+        " 3.10, inclusive."
     )
-    sys.exit(-1)
 
 setup(
     name="latch",
     version="v2.22.1",
     author_email="kenny@latch.bio",
-    description="The Latchbio SDK",
+    description="The Latch SDK",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.8",
+    python_requires=">=3.8,<3.11",
     entry_points={
         "console_scripts": [
             "latch=latch_cli.main:main",
         ]
     },
     install_requires=[
-        "awscli==1.25.22",
         "asyncssh==2.12.0",
         "aioconsole==0.5.1",
         "kubernetes>=24.2.0",
@@ -36,23 +34,21 @@ setup(
         "docker>=5.0",
         "paramiko>=2.11.0",
         "scp>=0.14.0",
-        "boto3>=1.24.22",
+        "boto3>=1.26.0",
         "tqdm>=4.63.0",
-        "lytekit==0.14.11",
+        "lytekit==0.14.12",
         "lytekitplugins-pods==0.4.0",
         "typing-extensions==4.5.0",
         "apscheduler==3.9.1",
-        "uvloop==0.17.0",
         "websockets==10.3",
-        "prompt-toolkit==3.0.33",
         "watchfiles==0.18.1",
         "gql==3.4.0",
         "graphql-core==3.2.3",
         "requests-toolbelt==0.10.1",
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
