c863274d0baf5ce2dfbd849158e37445206c4aa6
Kenny Workman <kennyworkman@sbcglobal.net>
2023-06-24 15:59:34 -0700

    remove websocket import and new release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if cur_ver < (3, 8) or cur_ver > (3, 10):
 
 setup(
     name="latch",
-    version="v2.24.3",
+    version="v2.24.4",
     author_email="kenny@latch.bio",
     description="The Latch SDK",
     packages=find_packages(),
