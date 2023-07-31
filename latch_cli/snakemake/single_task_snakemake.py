8eb6acb3088991acf5fde7012e1d2c9abc757c6d
maximsmol <maximsmol@gmail.com>
2023-07-31 11:15:25 -0700

    try commenting-out every line in a token
    Signed-off-by: maximsmol <maximsmol@gmail.com>

--- latch_cli/snakemake/single_task_snakemake.py
+++ latch_cli/snakemake/single_task_snakemake.py
@@ -228,7 +228,7 @@ class SkippingRule(Rule):
             return super().block_content(token)
 
         for t in super().block_content(token):
-            yield t.replace("\n", "\n#")
+            yield t.replace("\n", "\n# ")
 
 
 Python.subautomata["rule"] = SkippingRule
