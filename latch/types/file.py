f86459caf385ad63327850cc7c4e5ce33b50d4d7
Ayush Kamat <ayush@latch.bio>
2023-10-26 12:49:11 -0700

    rename some things
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/types/file.py
+++ latch/types/file.py
@@ -14,7 +14,7 @@ from flytekit.types.file.file import FlyteFile, FlyteFilePathTransformer
 from latch_sdk_gql.execute import execute
 from typing_extensions import Annotated
 
-from latch.types.utils import _format_path, _is_absolute_node_path, _is_valid_url
+from latch.types.utils import format_path, is_absolute_node_path, is_valid_url
 from latch_cli.utils.path import normalize_path
 
 
@@ -75,7 +75,7 @@ class LatchFile(FlyteFile):
 
         self._path_generated = False
 
-        if _is_valid_url(self.path) and remote_path is None:
+        if is_valid_url(self.path) and remote_path is None:
             self._remote_path = str(path)
         else:
             self._remote_path = None if remote_path is None else str(remote_path)
@@ -94,7 +94,7 @@ class LatchFile(FlyteFile):
                     and ctx.inspect_objects_only is False
                 ):
                     local_path_hint = self._remote_path
-                    if _is_absolute_node_path.match(self._remote_path) is not None:
+                    if is_absolute_node_path.match(self._remote_path) is not None:
                         data = execute(
                             gql.gql("""
                             query getName($argPath: String!) {
@@ -154,17 +154,17 @@ class LatchFile(FlyteFile):
 
     def __repr__(self):
         if self.remote_path is None:
-            return f"LatchFile({repr(_format_path(self.local_path))})"
+            return f"LatchFile({repr(format_path(self.local_path))})"
 
         return (
             f"LatchFile({repr(self.path)},"
-            f" remote_path={repr(_format_path(self.remote_path))})"
+            f" remote_path={repr(format_path(self.remote_path))})"
         )
 
     def __str__(self):
         if self.remote_path is None:
             return "LatchFile()"
-        return f"LatchFile({_format_path(self.remote_path)})"
+        return f"LatchFile({format_path(self.remote_path)})"
 
 
 LatchOutputFile = Annotated[
--- latch/types/utils.py
+++ latch/types/utils.py
@@ -7,7 +7,7 @@ import gql
 from latch_sdk_gql.execute import execute
 
 
-def _is_valid_url(raw_url: Union[str, Path]) -> bool:
+def is_valid_url(raw_url: Union[str, Path]) -> bool:
     """A valid URL (as a source or destination of a LatchFile) must:
     * contain a latch or s3 scheme
     * contain an absolute path
@@ -23,14 +23,12 @@ def _is_valid_url(raw_url: Union[str, Path]) -> bool:
     return True
 
 
-_is_absolute_node_path = re.compile(r"^(latch)?://(?P<node_id>\d+).node(/)?$")
+is_absolute_node_path = re.compile(r"^(latch)?://(?P<node_id>\d+).node(/)?$")
+old_style_path = re.compile(r"^(?:(?P<account_root>account_root)|(?P<mount>mount))")
 
-_old_path_expr = re.compile(r"^(?:(?P<account_root>account_root)|(?P<mount>mount))")
 
-
-def _format_path(path: str) -> str:
-    match = _is_absolute_node_path.match(path)
-
+def format_path(path: str) -> str:
+    match = is_absolute_node_path.match(path)
     if match is None:
         return path
 
@@ -46,19 +44,19 @@ def _format_path(path: str) -> str:
         {"id": node_id},
     )
 
-    raw_path: Optional[str] = data["ldataGetPath"]
-    if raw_path is None:
+    raw: Optional[str] = data["ldataGetPath"]
+    if raw is None:
         return path
 
-    path_split = raw_path.split("/")
+    parts = raw.split("/")
 
-    match = _old_path_expr.match(raw_path)
+    match = old_style_path.match(raw)
     if match is None:
         return path
 
     if match["mount"] is not None:
-        bucket = path_split[1]
-        key = "/".join(path_split[2:])
+        bucket = parts[1]
+        key = "/".join(parts[2:])
         return f"latch://{bucket}.mount/{key}"
 
     owner: Optional[str] = data["ldataOwner"]
@@ -66,7 +64,7 @@ def _format_path(path: str) -> str:
         return path
 
     if match["account_root"] is not None:
-        key = "/".join(path_split[2:])
+        key = "/".join(parts[2:])
         return f"latch://{owner}.account/{key}"
 
     return path
