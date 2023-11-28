262ce7c8f030ece76475ad249487f439400841f7
Ayush Kamat <ayush@latch.bio>
2023-11-28 11:22:05 -0800

    redo
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/__init__.py
+++ latch/__init__.py
@@ -4,47 +4,66 @@ A commandline toolchain to define and register serverless workflows with the
 Latch platform.
 """
 
+import importlib
+import sys
 import warnings
-from typing import Any, Dict
+from textwrap import dedent
+from types import ModuleType
+from typing import Callable
 
-from latch.functions.operators import (
-    combine,
-    group_tuple,
-    inner_join,
-    latch_filter,
-    left_join,
-    outer_join,
-    right_join,
-)
-from latch.resources.conditional import create_conditional_section
-from latch.resources.map_tasks import map_task
-from latch.resources.reference_workflow import workflow_reference
-from latch.resources.tasks import (
-    custom_memory_optimized_task,
-    custom_task,
-    large_gpu_task,
-    large_task,
-    medium_task,
-    small_gpu_task,
-    small_task,
-)
-from latch.resources.workflow import workflow
-
-_deprecation_version = "3.0.0"
-
-
-def _warn(name: str, import_path: str):
-    warnings.warn(
-        f"Importing `{name}` directly from `latch` is deprecated, and will"
-        f" be removed in version {_deprecation_version}.\n\n    Please use"
-        f" the full import `from {import_path} import {name}`\n",
-        DeprecationWarning,
-    )
+_imports = {
+    "latch.functions.operators": [
+        "combine",
+        "group_tuple",
+        "inner_join",
+        "latch_filter",
+        "left_join",
+        "outer_join",
+        "right_join",
+    ],
+    "latch.resources.conditional": ["create_conditional_section"],
+    "latch.resources.map_tasks": ["map_task"],
+    "latch.resources.reference_workflow": ["workflow_reference"],
+    "latch.resources.tasks": [
+        "custom_memory_optimized_task",
+        "custom_task",
+        "large_gpu_task",
+        "large_task",
+        "medium_task",
+        "small_gpu_task",
+        "small_task",
+    ],
+    "latch.resources.workflow": ["workflow"],
+}
 
 
-def message(typ: str, data: Dict[str, Any]):
-    from latch.functions.messages import message as _message
+def deprecated(module: ModuleType, fn_name: str) -> Callable:
+    fn = getattr(module, fn_name)
 
-    _warn("message", "latch.functions.messages")
+    def new_fn(*args, **kwargs):
+        warnings.warn(
+            dedent(f"""
 
-    return _message(typ, data)
+            Importing `{fn_name}` directly from `latch` is deprecated. Please use the full import
+
+                from {module.__name__} import {fn_name}
+
+            This will be removed in version 3.0.0.
+            """),
+            DeprecationWarning,
+        )
+
+        return fn(*args, **kwargs)
+
+    return new_fn
+
+
+module = sys.modules[__name__]
+for module_name, fn_names in _imports.items():
+    imported = importlib.import_module(module_name)
+
+    for fn_name in fn_names:
+        setattr(module, fn_name, deprecated(imported, fn_name))
+
+
+__slots__ = sum(_imports.values(), start=[])
