5bb3b7f6a07b657b9ec68936db08db8b75e764e9
Ayush Kamat <ayush@latch.bio>
2023-12-16 13:34:36 -0800

    docs for rename_current_execution
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch/executions.py
+++ latch/executions.py
@@ -6,6 +6,11 @@ from latch_sdk_gql.execute import execute
 
 
 def rename_current_execution(name: str):
+    """Rename the current execution.
+
+    Useful for naming executions based on inputs. If this function is called
+    outside of an execution context, it is a noop.
+    """
     token = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID", None)
     if token is None:
         # noop during local execution / testing
