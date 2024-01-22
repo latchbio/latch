3ec5d2f3a2bd1e4f8ba74f50108424e63e63bd2e
Taras Priadka <taras@latch.bio>
2024-01-22 10:56:38 -0800

    feat: delete local secrets and use get_auth_header

--- latch/functions/secrets.py
+++ latch/functions/secrets.py
@@ -3,7 +3,7 @@ import os
 from latch_sdk_config.latch import config
 
 from latch_cli.tinyrequests import post
-from latch_cli.utils import current_workspace, retrieve_or_login
+from latch_cli.utils import current_workspace, get_auth_header, retrieve_or_login
 
 
 def get_secret(secret_name: str):
@@ -20,32 +20,14 @@ def get_secret(secret_name: str):
         >>> get_secret("test-secret")
         "test-value-123"
     """
-    execution_token = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID")
-    if execution_token is None:
-        return _get_secret_local(secret_name)
 
     resp = post(
         url=config.api.user.get_secret,
         json={
             "name": secret_name,
-        },
-        headers={"Authorization": f"Latch-Execution-Token {execution_token}"},
-    )
-
-    if resp.status_code != 200:
-        raise ValueError(resp.json()["error"])
-
-    return resp.json()["secret"]
-
-
-def _get_secret_local(secret_name: str):
-    resp = post(
-        url=config.api.user.get_secret,
-        json={
             "ws_id": current_workspace(),
-            "name": secret_name,
         },
-        headers={"Authorization": f"Bearer {retrieve_or_login()}"},
+        headers={"Authorization": get_auth_header()},
     )
 
     if resp.status_code != 200:
