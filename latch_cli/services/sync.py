5bf0333bf291b85da6de8fb5c91a04734960ef95
AidanAbd <aidan@latch.bio>
2024-04-02 09:45:15 -0700

    fix: errors for unsupported cases in latch sync

--- latch_cli/services/sync.py
+++ latch_cli/services/sync.py
@@ -10,6 +10,8 @@ import dateutil.parser as dp
 import gql
 from gql.transport.exceptions import TransportQueryError
 from latch_sdk_gql.execute import JsonValue, execute
+from latch_cli.utils.path import is_remote_path
+
 
 import latch.ldata._transfer.upload as _upl
 
@@ -300,9 +302,18 @@ def sync(
     delete: bool,
     ignore_unsyncable: bool,
 ):
+    if not is_remote_path(dest):
+        click.secho(f"`{dest}`: only local -> remote sync is supported", fg="red", bold=True)
+        sys.exit(1)
+
     srcs: Dict[str, Tuple[Path, os.stat_result]] = {}
     have_errors = False
     for x in srcs_raw:
+        if is_remote_path(x):
+            click.secho(f"`{x}`: only local -> remote sync is supported", fg="red", bold=True)
+            have_errors = True
+            continue
+
         p = Path(x)
         res = check_src(p)
         if res is None:
