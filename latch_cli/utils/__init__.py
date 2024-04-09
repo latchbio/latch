ffec17b00304b5eb10339decc2ca069be8c30d39
Ayush Kamat <ayush@latch.bio>
2024-04-09 11:41:26 -0700

    fix auth git weirdness
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch_cli/utils/__init__.py
+++ latch_cli/utils/__init__.py
@@ -84,49 +84,6 @@ def get_auth_header() -> str:
     return header
 
 
-def retrieve_or_login() -> str:
-    """Returns a valid JWT to access Latch, prompting a login flow if needed.
-
-    Returns:
-        A JWT
-    """
-
-    from latch_cli.services.login import login
-
-    token = user_config.token
-    if token == "":
-        token = login()
-    return token
-
-
-class AccountId(TypedDict):
-    id: str
-
-
-class AccountInfoCurrent(TypedDict):
-    accountInfoCurrent: AccountId
-
-
-def current_workspace() -> str:
-    ws = user_config.workspace_id
-
-    if ws == "":
-        execution_token = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID")
-        if execution_token is not None:
-            ws = execute(gql.gql("""
-                query accountInfoCurrentQuery {
-                    accountInfoCurrent {
-                        id
-                    }
-                }
-            """))["accountInfoCurrent"]["id"]
-        else:
-            ws = account_id_from_token(retrieve_or_login())
-        user_config.update_workspace(ws, "Personal Workspace")
-
-    return ws
-
-
 def sub_from_jwt(token: str) -> str:
     """Extract a user sub (UUID) from a JWT minted by auth0.
 
@@ -146,19 +103,6 @@ def sub_from_jwt(token: str) -> str:
             " and is not a valid token."
         )
     return sub
-
-
-def _normalize_remote_path(remote_path: str):
-    if remote_path.startswith("latch://"):
-        remote_path = remote_path[len("latch://") :]
-    if (
-        not remote_path.startswith("/")
-        and not remote_path.startswith("shared")
-        and not remote_path.startswith("account")
-    ):
-        remote_path = f"/{remote_path}"
-
-    return remote_path
 
 
 def _si_number_strings(num):
