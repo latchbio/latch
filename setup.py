82322ee785c7b7f10eedee6998b2f064381d9b17
Ayush Kamat <ayush@latch.bio>
2024-05-24 14:00:18 -0700

    upgrade docker dep per https://github.com/docker/docker-py/issues/3256
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if cur_ver < (3, 8) or cur_ver > (3, 11):
 
 setup(
     name="latch",
-    version="v2.45.0",
+    version="v2.45.1",
     author_email="kenny@latch.bio",
     description="The Latch SDK",
     packages=find_packages(),
@@ -29,7 +29,7 @@ setup(
         "pyjwt>=0.2.0",
         "requests>=2.28.1",
         "click>=8.0",
-        "docker>=5.0",
+        "docker>=7.1.0",
         "paramiko>=3.2.0",
         "scp>=0.14.0",
         "boto3>=1.26.0",
