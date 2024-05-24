0812ccbe2245325a10cf3781d956a8bfd2a657e0
Rahul Desai <rahul@latch.bio>
2024-05-24 15:08:11 -0700

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if cur_ver < (3, 8) or cur_ver > (3, 11):
 
 setup(
     name="latch",
-    version="v2.41.3.dev1",
+    version="v2.41.3.dev2",
     author_email="kenny@latch.bio",
     description="The Latch SDK",
     packages=find_packages(),
