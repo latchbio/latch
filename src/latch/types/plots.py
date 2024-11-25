97f8b45e331e8f4ef5936c1660d9f74fb5e4ae20
Ryan Teoh <ryan@latch.bio>
2024-11-25 11:10:08 -0800

    move serialization into child classes

--- src/latch/types/plots.py
+++ src/latch/types/plots.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from typing import Any, Optional, Union
-import orjson
 
 
 @dataclass(frozen=True)
@@ -9,42 +8,41 @@ class Widget:
     key: str
     value: Any
 
+    def asdict(self) -> dict:
+        return {f"{self.transform_id}/{self.key}": {"value": self.value}}
+
 
 @dataclass(frozen=True)
 class PlotsArtifactTemplate:
     template_id: str
     widgets: Optional[list[Widget]] = None
 
+    def asdict(self) -> dict:
+        template: dict[str, Union[str, dict[str, dict[str, Any]]]] = {
+            "id": self.template_id
+        }
+
+        if self.widgets is not None:
+            widget_values: dict[str, dict[str, Any]] = {}
+            for w in self.widgets:
+                widget_values[f"{w.transform_id}/{w.key}"] = {"value": w.value}
+
+            template["widgetValues"] = widget_values
+
+        return template
+
 
 @dataclass(frozen=True)
 class PlotsArtifactBindings:
     plot_templates: list[PlotsArtifactTemplate]
+
+    def asdict(self) -> dict:
+        return {"plotTemplates": [pt.asdict() for pt in self.plot_templates]}
 
 
 @dataclass(frozen=True)
 class PlotsArtifact:
     bindings: PlotsArtifactBindings
 
-    def asdict(self):
-        d = {}
-        bindings = d["bindings"] = {}
-        plot_templates = bindings["plotTemplates"] = []
-
-        for pt in self.bindings.plot_templates:
-            template: dict[str, Union[str, dict[str, dict[str, Any]]]] = {
-                "id": pt.template_id
-            }
-
-            if pt.widgets is not None:
-                widget_values: dict[str, dict[str, Any]] = {}
-                for w in pt.widgets:
-                    widget_values[f"{w.transform_id}/{w.key}"] = {"value": w.value}
-
-                template["widgetValues"] = widget_values
-
-            plot_templates.append(template)
-
-        return d
-
-    def to_json(self) -> str:
-        return orjson.dumps(self.asdict()).decode()
+    def asdict(self) -> dict:
+        return {"bindings": self.bindings.asdict()}
