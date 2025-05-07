c0aced32883365d98335e194596e6f01f38eefef
AidanAbd <aidan@latch.bio>
2025-05-07 12:53:30 -0700

    fix: rework without wf

--- src/latch_cli/services/launch_v2/__init__.py
+++ src/latch_cli/services/launch_v2/__init__.py
@@ -43,7 +43,7 @@ def launch(*, wf_name: str, params: dict[str, Any], version: Optional[str] = Non
         if description.get("idx") != 0:
             continue
 
-        raw_python_interface_with_defaults = description.get("python_interface_with_defaults")
+        raw_python_interface_with_defaults = description.get("__workflow_meta__", {}).get("meta", {}).get("python_interface")
         if raw_python_interface_with_defaults is not None:
             python_interface_with_defaults = cloudpickle.loads(base64.b64decode(raw_python_interface_with_defaults))
             break
@@ -51,9 +51,19 @@ def launch(*, wf_name: str, params: dict[str, Any], version: Optional[str] = Non
     if python_interface_with_defaults is None:
         raise ValueError("No python interface found -- re-register workflow with latest latch version in workflow Dockerfile")
 
-    print("flyte_interface_types")
-    print(flyte_interface_types)
-    return 1
+    for k, v in python_interface_with_defaults.items():
+        if k in params:
+            continue
+
+        if v[1] is not None:
+            params[k] = v[1]
+            continue
+
+        t = v[0]
+        if hasattr(t, "__origin__") and t.__origin__ is Union and type(None) in t.__args__:
+            params[k] = None
+        else:
+            raise ValueError(f"Required parameter '{k}' not provided in params")
 
     ctx = FlyteContextManager.current_context()
     assert ctx is not None
@@ -62,8 +72,9 @@ def launch(*, wf_name: str, params: dict[str, Any], version: Optional[str] = Non
         ctx,
         incoming_values=params,
         flyte_interface_types=flyte_interface_types,
-        native_types=_unpickled,
+        native_types={
+            k: v[0] for k, v in python_interface_with_defaults.items()
+        },
     )
-
 
     return launch_workflow(token, wf_id, {k: gpjson.MessageToDict(v.to_flyte_idl()) for k, v in fixed_literals.items()})
