fd2bdb4758e7db5e6b28631583bda9ef45650460
Ayush Kamat <ayush@latch.bio>
2025-08-01 11:18:33 -0700

    update serialization schem
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- src/latch/types/samplesheet_item.py
+++ src/latch/types/samplesheet_item.py
@@ -24,7 +24,7 @@ T = TypeVar("T", bound=_IsDataclass)
 
 @dataclass
 class SamplesheetItem(Generic[T]):
-    spec: T
+    data: T
 
     # None if the row was manually input / pulled in from LData
     record: Optional[RegistryRecord]
@@ -48,7 +48,7 @@ class SamplesheetItemTypeTransformer(TypeTransformer[SamplesheetItem[T]]):
                 "`SamplesheetItem` spec type must be a dataclass"
             )
 
-        if not isinstance(v, SamplesheetItem) or not isinstance(v.spec, a[0]):
+        if not isinstance(v, SamplesheetItem) or not isinstance(v.data, a[0]):
             raise TypeTransformerFailedError("`SamplesheetItem`s are incompatible")
 
     def get_literal_type(self, t: type[SamplesheetItem[T]]) -> LiteralType:
@@ -61,11 +61,8 @@ class SamplesheetItemTypeTransformer(TypeTransformer[SamplesheetItem[T]]):
 
         return LiteralType(
             record_type=RecordType([
-                *[
-                    RecordFieldType(f.name, TypeEngine.to_literal_type(f.type))
-                    for f in fields(a[0])
-                ],
-                RecordFieldType("_latch_internal_record_id", record_id_type),
+                RecordFieldType("data", TypeEngine.to_literal_type(a[0])),
+                RecordFieldType("record_id", record_id_type),
             ])
         )
 
@@ -90,21 +87,14 @@ class SamplesheetItemTypeTransformer(TypeTransformer[SamplesheetItem[T]]):
         return Literal(
             record=Record(
                 fields=[
-                    *[
-                        RecordField(
-                            f.name,
-                            TypeEngine.to_literal(
-                                ctx,
-                                getattr(python_val.spec, f.name),
-                                # fixme(ayush): f.type can be a str - breaks in this case
-                                f.type,
-                                TypeEngine.to_literal_type(f.type),
-                            ),
-                        )
-                        for f in fields(a[0])
-                    ],
                     RecordField(
-                        "_latch_internal_record_id",
+                        "data",
+                        TypeEngine.to_literal(
+                            ctx, python_val.data, a[0], TypeEngine.to_literal_type(a[0])
+                        ),
+                    ),
+                    RecordField(
+                        "record_id",
                         TypeEngine.to_literal(
                             ctx,
                             record_id,
@@ -125,32 +115,52 @@ class SamplesheetItemTypeTransformer(TypeTransformer[SamplesheetItem[T]]):
         if lv.record is None:
             raise TypeTransformerFailedError("input `Literal` must be a `Record`")
 
-        spec_type = get_args(expected_python_type)[0]
+        data_literal: Optional[Literal] = None
+        record_literal: Optional[Literal] = None
+
+        field: RecordField
+        for field in lv.record.fields:
+            if field.key == "data":
+                data_literal = field.value
+                continue
+
+            if field.key == "record_id":
+                record_literal = field.value
+                continue
+
+            raise TypeTransformerFailedError(
+                f"invalid key in SamplesheetItem - {field.key}"
+            )
+
+        if data_literal is None or data_literal.record is None:
+            raise TypeTransformerFailedError("SamplesheetItem missing 'data' key")
+        if record_literal is None:
+            raise TypeTransformerFailedError("SamplesheetItem missing 'record_id' key")
+
+        data_type = get_args(expected_python_type)[0]
 
         by_name: dict[str, RecordField] = {}
-        for field in lv.record.fields:
+        for field in data_literal.record.fields:
             by_name[field.key] = field
 
         python_values: dict[str, object] = {}
-        for field in fields(spec_type):
-            if field.name not in by_name:
-                raise TypeTransformerFailedError(
-                    f"field `{field.name}` missing in record"
-                )
+        for f in fields(data_type):
+            if f.name not in by_name:
+                raise TypeTransformerFailedError(f"field {f.name!r} missing in record")
 
-            python_values[field.name] = TypeEngine.to_python_value(
-                ctx, by_name[field.name].value, field.type
+            python_values[f.name] = TypeEngine.to_python_value(
+                ctx, by_name[f.name].value, f.type
             )
 
         record_id: Optional[str] = TypeEngine.to_python_value(
-            ctx, by_name["_latch_internal_record_id"].value, Optional[str]
+            ctx, record_literal, Optional[str]
         )
 
         record: Optional[RegistryRecord] = None
         if record_id is not None:
             record = RegistryRecord(id=record_id)
 
-        return expected_python_type(spec=spec_type, record=record)
+        return expected_python_type(data=data_type(**python_values), record=record)
 
 
 TypeEngine.register(SamplesheetItemTypeTransformer())
